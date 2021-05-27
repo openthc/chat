91fd2a1d445a4ab7ddd860b9d780b6afa70d6a96
openthc <code@openthc.com>
2021-05-27 18:59:51 +0000

    drop salt, main oauth_provider func

--- boot.php
+++ boot.php
@@ -6,8 +6,34 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 define('APP_ROOT', __DIR__);
-define('APP_SALT', 'c7527f87015dbb20a8348da3f1fccaba7423884f13cb6acb58e7c23e44e6f995');
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
 \OpenTHC\Config::init(__DIR__);
+
+function _oauth_provider()
+{
+	$cfg = \OpenTHC\Config::get('openthc/sso');
+
+	$r = $_GET['r'];
+	switch ($r) {
+	case '1':
+	case 'r':
+		$r = $_SERVER['HTTP_REFERER'];
+		break;
+	}
+
+	$url = sprintf('https://%s/auth/back?%s', $_SERVER['SERVER_NAME'], http_build_query(array('r' => $r)));
+	$url = trim($url, '?');
+	$ocp = new \League\OAuth2\Client\Provider\GenericProvider([
+		'clientId' => $cfg['public'],
+		'clientSecret' => $cfg['secret'],
+		'redirectUri' => $url,
+		'urlAuthorize' => sprintf('https://%s/oauth2/authorize', $cfg['hostname']),
+		'urlAccessToken' => sprintf('https://%s/oauth2/token', $cfg['hostname']),
+		'urlResourceOwnerDetails' => sprintf('https://%s/oauth2/profile', $cfg['hostname']),
+		'verify' => true
+	]);
+
+	return $ocp;
+}
