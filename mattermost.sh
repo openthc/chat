63cdd8b50950fce1afeea31317bc4bf575856197
openthc <code@openthc.com>
2025-03-24 19:58:31 +0000

    start/stop script

---
+++ mattermost.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+#
+# Start Mattermost Server
+#
+
+set -o errexit
+set -o nounset
+
+f=$(readlink -f "$0")
+d=$(dirname "$f")
+
+cd "$d/mattermost/" || exit 1
+
+action="${1:-start}"
+case "$action" in
+start)
+	echo "START"
+	# exec \
+	nohup \
+		./bin/mattermost \
+		>"./mattermost.log" \
+		2>&1 \
+		&
+
+	echo "pid:$!"
+
+	;;
+
+stop)
+	echo "STOP"
+
+	pid=$(pidof mattermost)
+	if [ -n "$pid" ]
+	then
+		kill $pid
+	else
+		echo "No PID"
+	fi
+
+	;;
+
+esac
