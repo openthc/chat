7ef56000108ce2e7ecd73a4bccd688946b834a60
openthc <code@openthc.com>
2025-04-10 18:28:13 +0000

    dbc helper, jsRedirect helper

--- boot.php
+++ boot.php
@@ -11,19 +11,27 @@ require_once(__DIR__ . '/vendor/autoload.php');
 
 \OpenTHC\Config::init(__DIR__);
 
-function _oauth_provider()
+/**
+ * Database Conneciton
+ */
+function _dbc()
+{
+	static $dbc;
+	if (empty($dbc)) {
+		$dsn = \OpenTHC\Config::get('database/chat/dsn');
+		$dbc = new \Edoceo\Radix\DB\SQL($dsn);
+	}
+	return $dbc;
+}
+
+/**
+ * oAuth Provider Factory
+ */
+function _oauth_provider($s)
 {
 	$cfg = \OpenTHC\Config::get('openthc/sso');
 
-	$r = $_GET['r'];
-	switch ($r) {
-	case '1':
-	case 'r':
-		$r = $_SERVER['HTTP_REFERER'];
-		break;
-	}
-
-	$url = sprintf('https://%s/auth/back?%s', $_SERVER['SERVER_NAME'], http_build_query(array('r' => $r)));
+	$url = sprintf('https://%s/auth/back?_=%s', $_SERVER['SERVER_NAME'], $s);
 	$url = trim($url, '?');
 	$ocp = new \League\OAuth2\Client\Provider\GenericProvider([
 		'clientId' => $cfg['client-pk'],
@@ -36,4 +44,35 @@ function _oauth_provider()
 	]);
 
 	return $ocp;
+}
+
+/**
+ * Copied from www-com Project
+ * Wrapper to Draw Friendly JavaScript Redirector
+ * Have to go this way so that SameSite=Strict works properly
+ * @see https://www.nogginbox.co.uk/blog/strict-cookies-not-sent-by-request
+ */
+function doJavaScriptRedirect($url) : void
+{
+	$html = <<<HTML
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset=utf-8>
+		<title>Redirecting...</title>
+	</head>
+	<body>
+	<script>
+	window.localStorage.setItem('__landingPageSeen__', true);
+	window.localStorage.setItem('debug', undefined);
+	window.localStorage.setItem('was_notified_of_login', true)
+	window.localStorage.setItem('__announcement__Preview Mode: Email notifications have not been configured.', true);
+	</script>
+	<script>window.location = "$url";</script>
+	</body>
+	</html>
+	HTML;
+
+	__exit_html($html);
+
 }
